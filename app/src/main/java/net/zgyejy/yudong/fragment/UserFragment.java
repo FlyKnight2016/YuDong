817e22bfa04f893345ab21fa203fec69d3a39751
FlyKnight2016 <191663242@qq.com>
2016-10-18 12:49:44 +0800

    完成了个人中心的基本框架

--- app/src/main/java/net/zgyejy/yudong/fragment/UserFragment.java
+++ app/src/main/java/net/zgyejy/yudong/fragment/UserFragment.java
@@ -5,11 +5,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import net.zgyejy.yudong.R;
+import net.zgyejy.yudong.view.XCRoundRectImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class UserFragment extends Fragment {
+
+    @BindView(R.id.iv_userPortrait)
+    XCRoundRectImageView ivUserPortrait;
+    @BindView(R.id.tv_userName)
+    TextView tvUserName;
+    @BindView(R.id.tv_user_myBalance)
+    TextView tvUserMyBalance;
+    @BindView(R.id.tv_user_myCoupons)
+    TextView tvUserMyCoupons;
+    @BindView(R.id.tv_user_myPoints)
+    TextView tvUserMyPoints;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +37,30 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick({R.id.ll_user_myBalance, R.id.ll_user_myCoupons, R.id.ll_user_myPoints, R.id.ll_user_myOrders, R.id.ll_user_myCoupons2, R.id.ll_user_integralMall, R.id.ll_user_actCenter, R.id.ll_user_normalProblem})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ll_user_myBalance:
+                break;
+            case R.id.ll_user_myCoupons:
+                break;
+            case R.id.ll_user_myPoints:
+                break;
+            case R.id.ll_user_myOrders:
+                break;
+            case R.id.ll_user_myCoupons2:
+                break;
+            case R.id.ll_user_integralMall:
+                break;
+            case R.id.ll_user_actCenter:
+                break;
+            case R.id.ll_user_normalProblem:
+                break;
+        }
     }
 }
