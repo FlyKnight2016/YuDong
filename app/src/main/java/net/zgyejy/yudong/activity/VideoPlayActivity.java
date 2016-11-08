a4081c821122283157c5a440b4434920dd89226a
Your Name <you@example.com>
2016-11-08 16:53:20 +0800

    添加了音量和亮度滑动调节

--- app/src/main/java/net/zgyejy/yudong/activity/VideoPlayActivity.java
+++ app/src/main/java/net/zgyejy/yudong/activity/VideoPlayActivity.java
@@ -1,14 +1,20 @@
 package net.zgyejy.yudong.activity;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Display;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -95,6 +101,20 @@ public class VideoPlayActivity extends MyBaseActivity implements
 
         getPath();
 
+        mVolumeBrightnessLayout = findViewById(R.id.operation_volume_brightness);
+        mOperationBg = (ImageView) findViewById(R.id.operation_bg);
+        mOperationPercent = (ImageView) findViewById(R.id.operation_percent);
+
+        //获取系统最大音量
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        mMaxVolume = mAudioManager
+                .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        //设置视频准备完成、播放出错、播放完成的监听
+        mVideoView.setOnPreparedListener(this);
+        mVideoView.setOnErrorListener(this);
+        mVideoView.setOnCompletionListener(this);
+
         addOtherViewsToList();
 
         matchViewToOrientation();
@@ -106,6 +126,8 @@ public class VideoPlayActivity extends MyBaseActivity implements
         mVideoView.setMediaController(mMediaController);//绑定控制器
         mVideoView.setVideoURI(Uri.parse(path));//设置播放地址
         mVideoView.requestFocus();//取得焦点
+
+        mGestureDetector = new GestureDetector(this, new MyGestureListener());
 
 
         //加载评论列表数据
@@ -127,9 +149,130 @@ public class VideoPlayActivity extends MyBaseActivity implements
 
         //全屏时滑动屏幕调节亮度和音量
         if (fullscreen) {
+            if (mGestureDetector.onTouchEvent(event))
+                return true;
 
+            // 处理手势结束
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_UP:
+                    endGesture();
+                    break;
+            }
         }
         return super.onTouchEvent(event);
+    }
+    /** 手势结束 */
+    private void endGesture() {
+        mVolume = -1;
+        mBrightness = -1f;
+
+        // 隐藏
+        mDismissHandler.removeMessages(0);
+        mDismissHandler.sendEmptyMessageDelayed(0, 500);
+    }
+
+    /** 定时隐藏 */
+    private Handler mDismissHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            mVolumeBrightnessLayout.setVisibility(View.GONE);
+        }
+    };
+
+    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        /** 双击 */
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            if (!fullscreen) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                mMediaController.setFullScreenButton(R.drawable.esc_full_screen);
+            } else {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                mMediaController.setFullScreenButton(R.drawable.full_screen);
+            }
+            return true;
+        }
+
+        /** 滑动 */
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                float distanceX, float distanceY) {
+            float mOldX = e1.getX(), mOldY = e1.getY();
+            int y = (int) e2.getRawY();
+            Display disp = getWindowManager().getDefaultDisplay();
+            int windowWidth = disp.getWidth();
+            int windowHeight = disp.getHeight();
+
+            if (mOldX > windowWidth * 4.0 / 5)// 右边滑动
+                onVolumeSlide((mOldY - y) / windowHeight);
+            else if (mOldX < windowWidth / 5.0)// 左边滑动
+                onBrightnessSlide((mOldY - y) / windowHeight);
+
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+    }
+
+    /**
+     * 滑动改变声音大小
+     *
+     * @param percent
+     */
+    private void onVolumeSlide(float percent) {
+        if (mVolume == -1) {
+            mVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            if (mVolume < 0)
+                mVolume = 0;
+
+            // 显示
+            mOperationBg.setImageResource(R.drawable.video_volume_bg);
+            mVolumeBrightnessLayout.setVisibility(View.VISIBLE);
+        }
+
+        int index = (int) (percent * mMaxVolume) + mVolume;
+        if (index > mMaxVolume)
+            index = mMaxVolume;
+        else if (index < 0)
+            index = 0;
+
+        // 变更声音
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, index, 0);
+
+        // 变更进度条
+        ViewGroup.LayoutParams lp = mOperationPercent.getLayoutParams();
+        lp.width = findViewById(R.id.operation_full).getLayoutParams().width
+                * index / mMaxVolume;
+        mOperationPercent.setLayoutParams(lp);
+    }
+
+    /**
+     * 滑动改变亮度
+     *
+     * @param percent
+     */
+    private void onBrightnessSlide(float percent) {
+        if (mBrightness < 0) {
+            mBrightness = getWindow().getAttributes().screenBrightness;
+            if (mBrightness <= 0.00f)
+                mBrightness = 0.50f;
+            if (mBrightness < 0.01f)
+                mBrightness = 0.01f;
+
+            // 显示
+            mOperationBg.setImageResource(R.drawable.video_brightness_bg);
+            mVolumeBrightnessLayout.setVisibility(View.VISIBLE);
+        }
+        WindowManager.LayoutParams lpa = getWindow().getAttributes();
+        lpa.screenBrightness = mBrightness + percent;
+        if (lpa.screenBrightness > 1.0f)
+            lpa.screenBrightness = 1.0f;
+        else if (lpa.screenBrightness < 0.01f)
+            lpa.screenBrightness = 0.01f;
+        getWindow().setAttributes(lpa);
+
+        ViewGroup.LayoutParams lp = mOperationPercent.getLayoutParams();
+        lp.width = (int) (findViewById(R.id.operation_full).getLayoutParams().width * lpa.screenBrightness);
+        mOperationPercent.setLayoutParams(lp);
     }
 
     /**
@@ -280,8 +423,8 @@ public class VideoPlayActivity extends MyBaseActivity implements
 
     @Override
     public void onCompletion(MediaPlayer mp) {
-        showToast("准备好了");
-        mVideoView.start();
+        showToast("播放完成");
+
     }
 
     @Override
@@ -292,6 +435,7 @@ public class VideoPlayActivity extends MyBaseActivity implements
 
     @Override
     public void onPrepared(MediaPlayer mp) {
-        showToast("播放完成");
+        showToast("准备好了");
+        mVideoView.start();
     }
 }
